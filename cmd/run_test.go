88b843b79142f5b93989b7cd90035510967da24d
Nick Miyake <nmiyake@users.noreply.github.com>
2021-03-30 16:42:54 -0700

    Add failing test (#3)

---
+++ cmd/run_test.go
@@ -0,0 +1,13 @@
+// Copyright (c) 2018 Palantir Technologies Inc. All rights reserved.
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+package cmd
+
+import (
+	"testing"
+)
+
+func TestFail(t *testing.T) {
+	t.Fail()
+}
